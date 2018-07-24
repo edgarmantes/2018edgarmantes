d0561f3b01c3b13c51b6ef5238f205fcc27510a0
edgarmantes <edgar.mantes11@gmail.com>
2018-07-23 22:32:35 -0500

    Added the intro section with both sides

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,18 @@ import Link from 'gatsby-link'
 import Background from '../components/Background.js'
 
 const IndexPage = (props) => (
-	<div>
+	<div className="container full-height" >
 		<Background />
+		<section className="intro-section full-height">   
+			<div className="wrapper full-height">	
+				<div className="column-1 centered">	
+					<h1>Left side</h1>
+				</div>
+				<div className="column-1 centered">	
+					<h1>Right side</h1>
+				</div>
+			</div>
+		</section>
 	</div>
 )
 
